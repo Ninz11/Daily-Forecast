37597e3835f3424b935d74a4a37c90265b90c445
Ninz11 <148561307+Ninz11@users.noreply.github.com>
2023-11-14 07:26:11 +0000

    additional weather data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
 function retainWeatherInfo(response) {
-  console.log(response.data);
   let currentTemperature = document.querySelector("#city-weather-temp");
   let cityElement = document.querySelector("#city");
+  let cityDescription = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-mph");
+  let dayTime = document.querySelector("#day-time");
+  let date = new Date(response.data.time * 1000);
 
+  dayTime.innerHTML = currentDate(date);
   currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  cityDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}mph`;
+}
+
+function currentDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function search(city) {
